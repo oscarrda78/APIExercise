e67c2345c718f99c96358defcd4577b99c99b7ed
Oscar Rojas <oscarrda78@gmail.com>
2023-07-28 14:40:44 -0500

    OR:feature-03: Adding testing

---
+++ APIExercise.Tests/UnitTests/Controllers/ClientControllerTests.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+using Moq;
+using APIExercise.API.Controllers;
+using APIExercise.Core.Interfaces.Services;
+using APIExercise.Core.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+
+namespace APIExercise.Tests.UnitTests.Controllers
+{
+    public class ClientControllerTests
+    {
+        private Mock<IClientService> _mockService;
+        private Mock<IMapper> _mockMapper;
+        private Mock<ILogger<ClientController>> _mockLogger;
+        private ClientController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockService = new Mock<IClientService>();
+            _mockMapper = new Mock<IMapper>();
+            _mockLogger = new Mock<ILogger<ClientController>>();
+
+            _mockService.Setup(service => service.GetAllAsync())
+                .ReturnsAsync(new List<ClientReadDto>
+                {
+                    new ClientReadDto { Id = Guid.NewGuid() }
+                });
+
+            var clientId = Guid.NewGuid();
+            _mockService.Setup(service => service.GetByIdAsync(clientId))
+                .ReturnsAsync(new ClientReadDto { Id = clientId});
+
+            _mockMapper.Setup(mapper => mapper.Map<ClientReadDto>(It.IsAny<object>()))
+                .Returns(new ClientReadDto { });
+
+            _controller = new ClientController(_mockService.Object, _mockLogger.Object);
+        }
+
+        [Test]
+        public void GetAllClients_ReturnsOk_WithListOfClients()
+        {
+            var result = _controller.GetAllClients().Result as OkObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<List<ClientReadDto>>(result.Value);
+        }
+
+        [Test]
+        public void GetClientById_ReturnsClient_WhenIdIsValid()
+        {
+            var clientId = Guid.NewGuid();
+
+            var result = _controller.GetClientById(clientId).Result as OkObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ClientReadDto>(result.Value);
+            Assert.AreEqual(clientId, (result.Value as ClientReadDto).Id);
+        }
+
+        [Test]
+        public void CreateClient_ReturnsCreatedAtAction_WhenCreationIsSuccessful()
+        {
+            var clientCreateDto = new ClientCreateDto {};
+
+            var result = _controller.CreateClient(clientCreateDto).Result as CreatedAtActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ClientReadDto>(result.Value);
+        }
+
+        [Test]
+        public void UpdateClient_ReturnsNoContent_WhenUpdateIsSuccessful()
+        {
+            var clientId = Guid.NewGuid();
+            var clientUpdateDto = new ClientUpdateDto {  };
+
+            var result = _controller.UpdateClient(clientId, clientUpdateDto).Result as NoContentResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(204, result.StatusCode); 
+        }
+
+        [Test]
+        public void DeleteClient_ReturnsNoContent_WhenDeletionIsSuccessful()
+        {
+            var clientId = Guid.NewGuid();
+
+            var result = _controller.DeleteClient(clientId).Result as NoContentResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(204, result.StatusCode);
+        }
+    }
+}
---
+++ APIExercise.Tests/UnitTests/Services/ClientServiceTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using Moq;
+using APIExercise.Core.Interfaces.Repositories;
+using APIExercise.Core.Entities;
+using APIExercise.Core.DTOs;
+using AutoMapper;
+using APIExercise.Infrastructure.Implementations.Services;
+
+namespace APIExercise.Tests.UnitTests.Services
+{
+    public class ClientServiceTests
+    {
+        private Mock<IClientRepository> _mockRepository;
+        private Mock<IMapper> _mockMapper;
+        private ClientService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockRepository = new Mock<IClientRepository>();
+            _mockMapper = new Mock<IMapper>();
+
+            _mockRepository.Setup(repo => repo.GetAllAsync())
+                .ReturnsAsync(new List<Client>
+                {
+                    new Client { },
+                });
+            var clientId = Guid.NewGuid();
+            _mockRepository.Setup(repo => repo.GetByIdAsync(clientId))
+                .ReturnsAsync(new Client { });
+
+            _mockMapper.Setup(mapper => mapper.Map<ClientReadDto>(It.IsAny<object>()))
+                .Returns(new ClientReadDto { });
+
+            _service = new ClientService(_mockRepository.Object, _mockMapper.Object);
+        }
+
+        [Test]
+        public void GetAllAsync_ReturnsListOfClients()
+        {
+            var result = _service.GetAllAsync().Result;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<List<ClientReadDto>>(result);
+            Assert.IsTrue(result.Any()); 
+        }
+
+        [Test]
+        public void GetAsync_ReturnsClient_WhenIdIsValid()
+        {
+            var clientId = Guid.NewGuid();
+
+            var result = _service.GetByIdAsync(clientId).Result;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ClientReadDto>(result);
+        }
+
+        [Test]
+        public void AddAsync_AddsClientSuccessfully()
+        {
+            var clientCreateDto = new ClientCreateDto { };
+
+            var result = _service.AddAsync(clientCreateDto).Result;
+
+        }
+
+        [Test]
+        public void UpdateAsync_UpdatesClientSuccessfully()
+        {
+            var clientId = Guid.NewGuid();
+            var clientUpdateDto = new ClientUpdateDto {};
+
+            _service.UpdateAsync(clientId, clientUpdateDto);
+
+            _mockRepository.Verify(repo => repo.UpdateAsync(It.IsAny<Client>()), Times.Once);
+        }
+
+        [Test]
+        public void DeleteAsync_DeletesClientSuccessfully()
+        {
+            var clientId = Guid.NewGuid();
+
+            _service.DeleteAsync(clientId);
+
+            _mockRepository.Verify(repo => repo.DeleteAsync(It.IsAny<Guid>()), Times.Once);
+        }
+    }
+}
